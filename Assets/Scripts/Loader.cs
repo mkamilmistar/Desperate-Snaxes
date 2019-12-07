6142ccfe745b9baf18ee079a04e11a63fe23115b
Muhammad Kamil <39584996+mkamilmistar@users.noreply.github.com>
2019-12-07 21:55:54 +0700

    Project

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class Loader{
+	
+	public enum Scene{
+		GameScene,
+		Loading,
+        MainMenu,
+    }
+	
+	private static Action loaderCallbackAction;
+		
+	public static void Load(Scene scene){
+		loaderCallbackAction = () => {
+		SceneManager.LoadScene(scene.ToString());
+	};
+		SceneManager.LoadScene(Scene.Loading.ToString());
+
+}
+	
+	public static void LoaderCallback(){
+		if(loaderCallbackAction != null){
+			loaderCallbackAction();
+			loaderCallbackAction=null;
+		}
+	}
+		
+}
